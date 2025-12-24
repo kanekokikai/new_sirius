7f55a57fbc11a16e90aa391664740e9462947db5
kanekokikai <horiuchi@kanekokikai.co.jp>
2025-12-24 17:32:05 +0900

    リリース修正2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 // Dev はルート配信、ビルド時のみサブディレクトリ用のベースパスを付与
+// 配備先が https://kanekokikai-app.com/new_sirius/dist/ の場合に合わせる
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/new_sirius/dist/" : "/",
   plugins: [react()],
